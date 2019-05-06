7ab91ede3a90c2e1d5d22f6f4011a131845a5368
devHawk42 <devhawk42@gmail.com>
2019-05-06 05:05:55 -0300

    graphql-express

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _express = require("express");
+
+var app = (0, _express.express)();
+app.listen(3000, function () {
+  return console.log('Server on port 3000');
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import graphqlHTTP from 'express-graphql';
+
+const app = express();
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
+
+
+app.listen(3000, () => console.log('Server on port 3000'));
